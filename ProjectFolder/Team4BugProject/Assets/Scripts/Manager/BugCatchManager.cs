1470228ce8f13d1e40efd27a21b7dac5e0c0a00e
Wadaha <63825388+Jeong-Sanghun@users.noreply.github.com>
2021-10-09 02:11:14 +0900

    시원이를 위한 푸시

---
+++ ProjectFolder/Team4BugProject/Assets/Scripts/Manager/BugCatchManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugCatchManager : MonoBehaviour
+{
+    GameManager gameManager;
+    SaveDataClass saveData;
+    List<BugIngameClass> appearedBugList;
+    [SerializeField]
+    Transform handTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.inst;
+        saveData = gameManager.saveData;
+        appearedBugList = saveData.appearedBugList;
+        
+    }
+
+    public void On
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
