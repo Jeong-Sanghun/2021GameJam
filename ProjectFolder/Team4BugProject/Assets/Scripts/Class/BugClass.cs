5fd2997863c074cd5f9227a61f508f53b31d5af5
dynene <ekdlsb02@gmail.com>
2021-10-08 23:31:08 +0900

    클래스설계

---
+++ ProjectFolder/Team4BugProject/Assets/Scripts/Class/BugClass.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugClass
+{
+    int bugType; //0,1,2,3 or str??
+    int hp; //or float
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProjectFolder/Team4BugProject/Assets/Scripts/Class/DesireClass.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesireClass
+{
+    int desireType;
+    int desireValue;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
