246769c4204d8682e48585be2343e00959c5b2c3
J-siwon <tldnjs4210@gmail.com>
2021-10-09 17:59:31 +0900

    Joystick 수정

--- ProjectFolder/Team4BugProject/Assets/Siwon/Joystick.cs
+++ ProjectFolder/Team4BugProject/Assets/Siwon/Joystick.cs
@@ -30,10 +30,10 @@ public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointer
 
     public void OnDrag(PointerEventData eventData)
     {
-        Vector2 radius = outLine.sizeDelta / 2;
+        Vector2 radius = (outLine.sizeDelta *0.5f) / 2;
         //Debug.Log(eventData.position.x);
         Debug.Log((eventData.position - outLine.anchoredPosition) / (radius * canvas.scaleFactor));
-        input = new Vector2((eventData.position - outLine.anchoredPosition).x / (radius * canvas.scaleFactor).x - 6.3f, (eventData.position - outLine.anchoredPosition).y / (radius * canvas.scaleFactor).y - 3.6f);
+        input = new Vector2((eventData.position - outLine.anchoredPosition).x / (radius * canvas.scaleFactor).x - 6.4f, (eventData.position - outLine.anchoredPosition).y / (radius * canvas.scaleFactor).y - 3.8f);
         HandleInput(input.magnitude, input.normalized);
         handle.anchoredPosition = input * radius * handleRange;
     }
